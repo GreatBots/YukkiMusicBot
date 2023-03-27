79bbbd1c49adfe1a36d67d583ff91b2b1a75583b
Vivek Tp <Vivektvp1@gmail.com>
2023-03-27 19:23:46 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -65,10 +65,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/GlobalBotupdates"
+    "SUPPORT_CHANNEL", "https://t.me/+H-qLipS061A0ODhl"
 )  # Example:- https://t.me/TheYukki
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/GlobalBotChat"
+    "SUPPORT_GROUP", "https://t.me/+Ag9fOOONs2w2NmNl"
 )  # Example:- https://t.me/YukkiSupport
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
